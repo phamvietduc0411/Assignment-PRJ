0a2f215bbb29a090a7dc65628bce07e08853ab8e
trongminhSE172130 <156078684+trongminhSE172130@users.noreply.github.com>
2024-03-15 20:54:30 +0700

    update brandDTO

--- PRJ_ASSIGNMENT/prj301-Assignment/src/main/java/Model/Brands/BrandsDTO.java
+++ PRJ_ASSIGNMENT/prj301-Assignment/src/main/java/Model/Brands/BrandsDTO.java
@@ -11,5 +11,49 @@ package Model.Brands;
  */
 public class BrandsDTO {
     private int BrandID;
-    private String 
+    private String BrandName;
+    private String Contacts;
+    private String Region;
+    private String Address;
+
+    public int getBrandID() {
+        return BrandID;
+    }
+
+    public void setBrandID(int BrandID) {
+        this.BrandID = BrandID;
+    }
+
+    public String getBrandName() {
+        return BrandName;
+    }
+
+    public void setBrandName(String BrandName) {
+        this.BrandName = BrandName;
+    }
+
+    public String getContacts() {
+        return Contacts;
+    }
+
+    public void setContacts(String Contacts) {
+        this.Contacts = Contacts;
+    }
+
+    public String getRegion() {
+        return Region;
+    }
+
+    public void setRegion(String Region) {
+        this.Region = Region;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+    
 }
