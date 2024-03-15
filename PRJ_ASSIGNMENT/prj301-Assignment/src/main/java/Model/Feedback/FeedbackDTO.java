c033dbe2315b7c2153d1e6a12d3b9c2f4bc0e79b
trongminhSE172130 <156078684+trongminhSE172130@users.noreply.github.com>
2024-03-15 22:43:54 +0700

    Create FeedbackDTO.java

---
+++ PRJ_ASSIGNMENT/prj301-Assignment/src/main/java/Model/Feedback/FeedbackDTO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Feedback;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Acer
+ */
+public class FeedbackDTO {
+    private int FeedbackID;
+    private String detail;
+    private int Rating;
+    private Date feedbackDate;
+
+    public int getFeedbackID() {
+        return FeedbackID;
+    }
+
+    public void setFeedbackID(int FeedbackID) {
+        this.FeedbackID = FeedbackID;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getRating() {
+        return Rating;
+    }
+
+    public void setRating(int Rating) {
+        this.Rating = Rating;
+    }
+
+    public Date getFeedbackDate() {
+        return feedbackDate;
+    }
+
+    public void setFeedbackDate(Date feedbackDate) {
+        this.feedbackDate = feedbackDate;
+    }
+    
+}
